f08a4dfc64436aad0921a515535c0abf176a41a0
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-05-03 23:45:33 +0300

    solve lab

---
+++ math_series/math_series.py
@@ -0,0 +1,32 @@
+def fibonacci(num):
+    if num < 0 or type(num) != int:
+        return "Not int"
+    elif num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fibonacci(num-1) + fibonacci(num-2)
+
+
+def lucas(num):
+    if num < 0 or type(num) != int:
+        return "Not int"
+    elif num == 0:
+        return 2
+    elif num == 1:
+        return 1
+    else:
+        return lucas(num-1) + lucas(num-2)
+
+
+
+def sum_series(num, val1=0, val2=1):
+    if num< 0 or type(num) != int:
+        return "Not int"
+    elif num== 0:
+        return val1
+    elif num== 1:
+        return val2
+    else:
+        return sum_series(num-1,val1,val2) + sum_series(num-2,val1,val2)
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,5 +1,58 @@
 from math_series import __version__
+from math_series.math_series import *
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_fibonacci():
+    actual1= fibonacci(-10)
+    actual2= fibonacci(0)
+    actual3= fibonacci(1)
+    actual4= fibonacci(5)
+
+    expected1= "Not int"
+    expected2= 0
+    expected3= 1
+    expected4= 5
+
+    assert actual1==expected1
+    assert actual2==expected2
+    assert actual3==expected3
+    assert actual4==expected4
+
+def test_lucas():
+    actual1= lucas(-4)
+    actual2= lucas(0)
+    actual3= lucas(1)
+    actual4= lucas(5)
+
+    expected1= "Not int"
+    expected2= 2
+    expected3= 1
+    expected4= 11
+
+    assert actual1==expected1
+    assert actual2==expected2
+    assert actual3==expected3
+    assert actual4==expected4
+
+def test_sum_series():
+    actual1= sum_series(-4)
+    actual2= sum_series(0)
+    actual3= sum_series(1)
+    actual4= sum_series(5)
+    actual5= sum_series(5,10,10)
+
+    expected1= "Not int"
+    expected2= 0
+    expected3= 1
+    expected4= 5
+    expected5= 80
+
+    assert actual1==expected1
+    assert actual2==expected2
+    assert actual3==expected3
+    assert actual4==expected4
+    assert actual5==expected5
+
